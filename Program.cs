3d3d46c42c931d7caa9304f978996057476a53cc
SimranPannu <53317578+SimranPannu@users.noreply.github.com>
2019-12-03 20:18:53 -0500

    Finished project

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 
 namespace live_coding3
@@ -7,11 +7,27 @@ namespace live_coding3
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
             int [] array_one = { 1, 5, 2 };
             int [] array_two = { 3, 6, 4 };
-            int [] zipped_array = {};
-            zipped_array = array_one.Zip(array_two(array_one(a+b));
+            int [] zipped_array = new int[array_one.Length+array_two.Length];
+            int a = 0;
+            for (int k=0; k<zipped_array.Length; k++)
+            {
+                zipped_array[a]= array_one[k];
+                a++;
+                zipped_array[a] = array_two[k];
+                a++;
+
+            }
+            for (int k = 0; k < zipped_array.Length; k++)
+            {
+                Console.WriteLine(zipped_array[k]);
+            }
+
+
+
+            
         }
 
         
