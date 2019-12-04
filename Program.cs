0a0a297c16ddd1b7466e013c245bde9da766a440
SimranPannu <53317578+SimranPannu@users.noreply.github.com>
2019-12-03 19:15:00 -0500

    added first version

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace live_coding3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int [] array_one = { 1, 5, 2 };
+            int [] array_two = { 3, 6, 4 };
+            int [] zipped_array = {};
+            zipped_array = array_one.Zip(array_two(array_one(a+b));
+        }
+
+        
+    }
+}
